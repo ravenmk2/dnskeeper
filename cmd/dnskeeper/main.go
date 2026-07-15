241a87f398a796f55fdcbc00c8ee638e0db42557
Raven <357679715@qq.com>
2026-07-15 20:57:19 +0800

    feat(cmd): 添加程序入口 main 包
    修复 .gitignore 将 dnskeeper 改为 /dnskeeper 锚定根目录，避免误忽略 cmd/dnskeeper 包

---
+++ cmd/dnskeeper/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ravenmk2/dnskeeper/internal/app"
+	"github.com/ravenmk2/dnskeeper/internal/config"
+	"github.com/ravenmk2/dnskeeper/internal/log"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	configPath := flag.String("config", "./config.toml", "path to config file")
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		logrus.Fatalf("load config: %v", err)
+	}
+
+	logger := log.Setup(cfg.Log.Level, os.Stdout)
+
+	application, err := app.New(cfg, logger)
+	if err != nil {
+		logger.Fatalf("init app: %v", err)
+	}
+
+	seedCtx, seedCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	if err := application.SeedAdmin(seedCtx); err != nil {
+		logger.WithError(err).Warn("seed admin user failed")
+	}
+	seedCancel()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := application.Run(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.WithError(err).Fatal("server error")
+		}
+	}()
+
+	sig := <-sigCh
+	logger.Infof("received signal %s, shutting down", sig)
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownCancel()
+	if err := application.Shutdown(shutdownCtx); err != nil {
+		logger.WithError(err).Error("shutdown error")
+	}
+	logger.Info("server stopped")
+}
